55db482a81393545b490b931311f6d881996621f
Ken <396989681@qq.com>
2020-12-08 17:37:51 +0800

    base

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{ title }}
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent(() => {
+    const title = 'vue_cli_test'
+    return {
+        title
+    }
+})
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
